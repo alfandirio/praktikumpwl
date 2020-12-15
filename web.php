45393e0147e8e9f87a26451a81a444a9a22f5c37
alfandirio <73221556+alfandirio@users.noreply.github.com>
2020-12-15 16:02:04 +0700

    Add files via upload

---
+++ web.php
@@ -0,0 +1,42 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+use Illuminate\Http\Request;
+Route::get ('/home', 'homeController@home');
+Route::get ('/about', 'aboutController@about');
+Route::get ('/article/{page}', 'articleController@article');
+Route::get ('/kuis1/{page}', 'kuis1Controller@kuis1');
+Route::get('/manage', 'ArticleController@index')->name('manage');
+
+Route::get('/', function () {
+    return view('home');
+});
+Route::get('/home', 'homeController@index');
+Route::get('/article/add','ArticleController@add'); 
+Route::post('/article/create','ArticleController@create'); 
+Route::get('/article/edit/{id}','ArticleController@edit'); 
+Route::post('/article/update/{id}','ArticleController@update'); 
+Route::get('/article/delete/{id}','ArticleController@delete');
+
+Gate::define('manage-articles', function($user){      
+     return $user->roles == "Administrator"; }); 
+Gate::define('user-display', function($user){      
+      return $user->roles == "User"; 
+      Route::get('/article/cetak_pdf', 'ArticleController@cetak_pdf');
+
+      Route::post('/register', 'Api\AuthController@register');
+      Route::post('/login', 'Api\AuthController@login');
+     Route::apiResource('/ceo', 'Api\CEOController')-
+     >middleware('auth:api');
+     }); 
+});
+});
